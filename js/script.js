8bd474e1474732cb55716432ee29397826267d9e
si1verhoof <ftadsh@gmail.com>
2017-06-18 11:51:06 +0500

    корректировки js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-var mainNav = document.querySelector(".main-nav__mobile-vision");
-var navToggle = mainNav.querySelector(".toggler");
+var mainNav = document.querySelector(".main-nav__mobile-menu-vision");
+var navToggle = document.querySelector(".toggler");
 var navClose = mainNav.querySelector(".main-nav__close-menu");
 var navOpen = mainNav.querySelector(".toggler--open");
 
 navToggle.classList.remove("main-nav__toggler--hidden");
-mainNav.classList.add("main-nav__mobile-menu-vision--none");
 
 navToggle.addEventListener("click", function() {
   mainNav.classList.toggle("main-nav__mobile-menu-vision--none");
