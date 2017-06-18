4f717790bc78c1f072ce7c549d8993f072f9b79b
si1verhoof <ftadsh@gmail.com>
2017-06-18 11:23:43 +0500

    Промежуточный итог

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 var mainNav = document.querySelector(".main-nav__mobile-vision");
-var navToggle = mainNav.querySelector(".main-nav__toggler");
+var navToggle = mainNav.querySelector(".toggler");
 var navClose = mainNav.querySelector(".main-nav__close-menu");
+var navOpen = mainNav.querySelector(".toggler--open");
 
 navToggle.classList.remove("main-nav__toggler--hidden");
-navClose.classList.remove("main-nav__close-menu--hidden");
 mainNav.classList.add("main-nav__mobile-menu-vision--none");
 
 navToggle.addEventListener("click", function() {
-  mainNav.classList.remove("main-nav__mobile-menu-vision--none");
+  mainNav.classList.toggle("main-nav__mobile-menu-vision--none");
+  navToggle.classList.toggle("toggler--open");
+  navToggle.classList.toggle("toggler--close");
 })
