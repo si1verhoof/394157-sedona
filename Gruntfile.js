7f15adde5802383a0d0ff146f8a817a4d1567496
si1verhoof <ftadsh@gmail.com>
2017-06-23 11:42:47 +0500

    Исправлена автоматизация

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,7 +74,7 @@ module.exports = function(grunt) {
           report: "gzip"
         },
         files: {
-          "build/css/style.min.css": ["css/style.css"]
+          "build/css/style.min.css": ["build/css/style.css"]
         }
       }
     },
